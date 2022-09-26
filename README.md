eaf3d3b0071cf1d68dd75d6226fb7a8f512f1555
Lucien-M <mail@gmail.com>
2022-09-25 23:41:02 -0400

    Adding other sounds

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # FlappyBird
+
+THis game involves a bird that the player can control.
+
+The player has to save the bird from hitting the pillars.
+
+Each time the bird passes through the pillars, the player scored a point, the score gets increased by one point.
+
+The game restarts when the bird hits the pillars, or falls down due to gravity
+
+What we need for the game:
+
+A bird
+
+Pillars.
+
+Pillars come at the bird in the direction the bird is flying
--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var ctx = c.getContext('2d');
         bird.src = "bird.png" 
         var scor = new Audio();
        scor.src = "score.wav";
-       var hit = new Audio();
-       hit.src = "hit.wav";
+       var die = new Audio();
+       die.src = "die.wav";
        var flap = new Audio();
        flap.src = "flap.wav";
        var hit = new Audio();
@@ -32,11 +32,11 @@ var ctx = c.getContext('2d');
             pillarX -= 8;
             pillarX < -pillarWidth && ((pillarX = canvasSize), (NorthPillarBottomY = gap * Math.random()));
             ctx.fillRect(pillarX, 0, pillarWidth, NorthPillarBottomY); //Draw top pipe
-            ctx.fillRect(pillarX, NorthPillarBottomY + gap, pillarWidth, canvasSize); //Draw bottom pipe
+            ctx.fillRect(pillarX, NorthPillarBottomY + gap, pillarWidth, canvasSize); //Draw bottom pipe 
             ctx.fillStyle = "black"
             ctx.fillText(`Score: ${score}`, 20, 35); score++;scor.play();
             bestScore = bestScore < score ? score : bestScore;
             ctx.fillText(`Best: ${bestScore}`, 20, 60);
             (((bY < NorthPillarBottomY || bY > NorthPillarBottomY + gap) && pillarX < bSize * (1.5)) || bY > canvasSize) && // Bird falls off screen
-            ((bDY = 0), (bY = 250), (pillarX = canvasSize), (score = 0), hit.play()); // Bird died
+            ((bDY = 0), (bY = 250), (pillarX = canvasSize), (score = 0), die.play()); // Bird died
         }, interval)                     
